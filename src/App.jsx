12a2c2492459e331a07052ecf216b3208714507c
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-11-26 01:46:08 +0600

    Added Want to cook section and style them not fully functional will work for next day

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "./components/Banner";
 import Navbar from "./components/Navbar";
 import OurRecipes from "./components/OurRecipes";
@@ -5,6 +6,19 @@ import Recipes from "./components/Recipes";
 import Sidebar from "./components/Sidebar";
 
 const App = () => {
+  const [recipeQueue,setRecipeQueue] = useState([]);
+
+  const addRecipeQueue = (recipes) => {
+    const isExist = recipeQueue.find(
+      (prevRecipe) => prevRecipe.recipe_id === recipes.recipe_id
+    );
+    if(!isExist){
+      setRecipeQueue([...recipeQueue, recipes]);
+    }
+    else {
+      alert("Recipe Already Exists")
+    }
+  }
   return (
     <div className="w-11/12 mx-auto">
       {/* Navbar Section */}
@@ -15,9 +29,9 @@ const App = () => {
       <OurRecipes/>
       <section className="flex flex-col md:flex-row gap-6 mt-20">
           {/* Cards Section */}
-          <Recipes/>
+          <Recipes addRecipeQueue={addRecipeQueue}/>
           {/* SideBar */}
-          <Sidebar/>
+          <Sidebar recipeQueue={recipeQueue}/>
       </section>
     </div>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,40 @@
-const Sidebar = () => {
-    return (
-        <div className="md:w-1/3">
-            <h1>SIde Bar</h1>
+const Sidebar = ({ recipeQueue }) => {
+  return (
+    <div className="md:w-1/3 border h-96 p-3 rounded-xl mt-6">
+      <h1 className="font-bold text-2xl text-center">
+        Want to Cook:{recipeQueue.length}
+      </h1>
+      {recipeQueue.map((recipe) => (
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr className="bg-base-200 font-semibold text-gray-600">
+                <th>{recipe.recipe_id}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+                <div className="flex-end">
+                  <button className="flex items-center py-2 px-5 bg-[#0BE58A] border-2 text-black mt-1 mb-1 rounded-full">
+                    Prepare
+                  </button>
+                </div>
+              </tr>
+            </tbody>
+          </table>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Sidebar;
