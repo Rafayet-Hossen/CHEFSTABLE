625ea27139aff7e2404e828d7583edbd1895794c
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-11-25 23:28:43 +0600

    Setup project and edit banner section and navbar for this website

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Banner from "./components/Banner";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      {/* Navbar Section */}
+      <Navbar/>
+      {/* Banner Section */}
+      <Banner/>
+      {/* Recepies Section */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+      <div className='flex justify-center items-center mt-5'>
+        <img className='rounded-xl'
+          src="https://i.postimg.cc/XYWhxMsJ/Yellow-Creative-Noodle-Food-Promotion-Banner.png"
+          alt=""
+        />
+      </div>
+    );
+};
+
+export default Banner;
